5df79dff647be2a40e4fd57bcd9109977afd1e9a
jeongjiwoo-include <jjw89506343@gmail.com>
2021-03-21 14:49:40 +0900

    Tab Bottom navigation

--- src/RootNavigation.tsx
+++ src/RootNavigation.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {InputStartPoint} from './screen/InputStartPoint';
 import {ShowRoute} from './screen/ShowRoute';
 import DetectBeacon from './screen/DetectBeacon';
@@ -8,16 +9,10 @@ import {STRING} from './assets/string';
 
 export const RootNavigation = () => {
   const Stack = createStackNavigator();
+  const Tab = createBottomTabNavigator();
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen
-          name={STRING.NAVIGATION.INPUT_START}
-          component={InputStartPoint}
-          options={{
-            header: () => null,
-          }}
-        />
         <Stack.Screen
           name={STRING.NAVIGATION.SHOW_ROUTE}
           component={ShowRoute}
@@ -26,12 +21,25 @@ export const RootNavigation = () => {
           }}
         />
         <Stack.Screen
-          name={STRING.NAVIGATION.DETECT_BEACONE}
-          component={DetectBeacon}
+          name={' '}
           options={{
-            header: () => null,
-          }}
-        />
+            headerStyle: {height: 0},
+          }}>
+          {() => (
+            <Tab.Navigator>
+              <Tab.Screen
+                name={STRING.NAVIGATION.INPUT_START}
+                component={InputStartPoint}
+                options={{}}
+              />
+              <Tab.Screen
+                name={STRING.NAVIGATION.DETECT_BEACONE}
+                component={DetectBeacon}
+                options={{}}
+              />
+            </Tab.Navigator>
+          )}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
