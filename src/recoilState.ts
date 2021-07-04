f24f8ace9131c743da66888a14c42bf01893ac26
hongchal <chal405@naver.com>
2021-07-04 17:20:40 +0900

    지하철 탑승 인식 완료

--- src/recoilState.ts
+++ src/recoilState.ts
@@ -1,7 +1,12 @@
-import {atom} from "recoil";
+import {atom} from 'recoil';
 
-export const subwayLineState= atom({
-    key:'subwayLineState',
-    default : '',
-})
+export const subwayLineState = atom({
+  key: 'subwayLineState',
+  default: '',
+});
 //todo: 지하철경로 출력시 거쳐가야하는 호선 수를 Array로 넣어야함
+
+export const isClientRideState = atom<boolean>({
+  key: 'isClientRideState',
+  default: false,
+});
